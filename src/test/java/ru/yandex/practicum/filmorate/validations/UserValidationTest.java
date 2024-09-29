3dfbb0662e515ffd4187cbee876bda108531f34d
iozimniy <Kitriss@gmail.com>
2024-09-29 23:26:48 +0300

    clean tests code

--- src/test/java/ru/yandex/practicum/filmorate/validations/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validations/UserValidationTest.java
@@ -8,7 +8,7 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserValidationTest {
 
@@ -17,7 +17,7 @@ public class UserValidationTest {
 
     @Test
     public void createUserEmailWithoutDogValidationException() {
-        User user = new User(1L,"email", "ShockDog", "Ivan",
+        User user = new User(1L, "email", "ShockDog", "Ivan",
                 LocalDate.of(1990, 10, 26));
 
         assertThrows(ValidationException.class, () -> UserValidation.validateForCreate(user));
@@ -25,7 +25,7 @@ public class UserValidationTest {
 
     @Test
     public void createUserEmailIsBlankValidationException() {
-        User user = new User(1L,"   ", "ShockDog", "Ivan",
+        User user = new User(1L, "   ", "ShockDog", "Ivan",
                 LocalDate.of(1990, 10, 26));
 
         assertThrows(ValidationException.class, () -> UserValidation.validateForCreate(user));
@@ -33,28 +33,28 @@ public class UserValidationTest {
 
     @Test
     public void createUserLoginIsBlankValidationException() {
-        User user = new User(1L,"email@email", "  ", "Ivan",
+        User user = new User(1L, "email@email", "  ", "Ivan",
                 LocalDate.of(1990, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForCreate(user));
     }
 
     @Test
     public void createUserLoginContainsWhiteSpaceValidationException() {
-        User user = new User(1L,"email@email", "Shock Dog", "Ivan",
+        User user = new User(1L, "email@email", "Shock Dog", "Ivan",
                 LocalDate.of(1990, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForCreate(user));
     }
 
     @Test
     public void createUserBirthDayInFutureValidationException() {
-        User user = new User(1L,"email@email", "ShockDog", "Ivan",
+        User user = new User(1L, "email@email", "ShockDog", "Ivan",
                 LocalDate.of(2026, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForCreate(user));
     }
 
     @Test
     public void updateUserIdIsNullValidationException() {
-        User user = new User(null,"email@email", "ShockDog", "Ivan",
+        User user = new User(null, "email@email", "ShockDog", "Ivan",
                 LocalDate.of(2026, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForUpdate(user));
     }
@@ -62,14 +62,14 @@ public class UserValidationTest {
     @Test
     public void updateUserIdIsAbsentNotFoundException() {
         createUser();
-        User user = new User(25L,"email@email", "ShockDog", "Ivan",
+        User user = new User(25L, "email@email", "ShockDog", "Ivan",
                 LocalDate.of(1990, 10, 26));
         assertThrows(NotFoundException.class, () -> userController.update(user));
     }
 
     @Test
     public void updateUserLoginContainsWhiteSpaceValidationException() {
-        User user = new User(1L,"email@email", "Shock Dog", "Ivan",
+        User user = new User(1L, "email@email", "Shock Dog", "Ivan",
                 LocalDate.of(1990, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForUpdate(user));
     }
@@ -77,21 +77,21 @@ public class UserValidationTest {
 
     @Test
     public void updateUserLoginIsBlankValidationException() {
-        User user = new User(1L,"email@email", "  ", "Ivan",
+        User user = new User(1L, "email@email", "  ", "Ivan",
                 LocalDate.of(1990, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForUpdate(user));
     }
 
     @Test
     public void updateUserLoginBirthDayInFutureSpaceValidationException() {
-        User user = new User(1L,"email@email", "ShockDog", "Ivan",
+        User user = new User(1L, "email@email", "ShockDog", "Ivan",
                 LocalDate.of(2026, 10, 26));
         assertThrows(ValidationException.class, () -> UserValidation.validateForUpdate(user));
     }
 
     // вспомогательные методы
     private void createUser() {
-        testUser = new User(1L,"email@email", "ShockDog", "Ivan",
+        testUser = new User(1L, "email@email", "ShockDog", "Ivan",
                 LocalDate.of(1990, 10, 26));
         userController.create(testUser);
     }
