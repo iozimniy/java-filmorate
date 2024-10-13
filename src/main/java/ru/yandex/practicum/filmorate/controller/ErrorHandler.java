2cd2758ea0ab3bee75bc239fbf4749a6324fd819
iozimniy <Kitriss@gmail.com>
2024-10-13 23:51:01 +0300

    fx statusCode

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.ErrorResponse;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
@@ -12,12 +14,14 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 public class ErrorHandler {
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
       log.info("Отправлен ответ на сключения NotFoundException: {}", e.getMessage());
       return new ErrorResponse("Ошибка запроса", e.getMessage());
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
         log.info("Отправлен ответ на сключения ValidationException: {}", e.getMessage());
         return new ErrorResponse("Ошибка при валидации запроса", e.getMessage());
